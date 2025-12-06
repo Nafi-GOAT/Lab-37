fd9f95528a280efb81feb156905c5e662ed7a3ff
Nafi-GOAT <nafih2989@gmail.com>
2025-12-06 13:40:55 -0800

    Working on part Two. (Main Function)

--- lab-37-starter-1.cpp
+++ lab-37-starter-1.cpp
@@ -4,12 +4,13 @@
 //
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int sum_ascii(const string &s) {
     int total = 0;
-    for (char c : s) {
-        total += (int)c;
+    for (int i = 0; i < s.length(); i++) {
+        total += (int)s[i];
     }
     return total;
 }
@@ -29,10 +30,3 @@ int main() {
     return 0;
 }
 
-/* 
-These targets are present in the dataset and can be used for testing:
-536B9DFC93AF
-1DA9D64D02A0
-666D109AA22E
-E1D2665B21EA
-*/
